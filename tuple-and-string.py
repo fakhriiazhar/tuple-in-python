22600cbb896cf851386001315a8800f5bbbd8a3d
Fakhrii Azhar <154650463+fakhriiazhar@users.noreply.github.com>
2024-10-19 12:36:36 +0800

    Create tuple-and-string.py

---
+++ tuple-and-string.py
@@ -0,0 +1,7 @@
+kelas_a = ("ayu", "budi", "dimas", "riki", "yuyun")
+a = input("silahkan masukan nama siswa: ")
+if a in kelas_a:
+    print("anda masuk kelas A")
+else:
+    print("anda tidak masuk kelas A")
+    
