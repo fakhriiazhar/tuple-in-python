9f3aae014eb0d30d9a27fd25fc0046919e12682a
Fakhrii Azhar <154650463+fakhriiazhar@users.noreply.github.com>
2024-10-19 12:39:00 +0800

    Create method-tuple.py

---
+++ method-tuple.py
@@ -0,0 +1,6 @@
+num = (11, 12, 23, 31, 64, 23, 17, 12, 56, 12, 90, 95, 64)
+print("Jumlah angka 12 dalam tuple = %d angka" % num.count(12))
+
+num = (11, 12, 23, 31, 64, 23, 17, 12, 56, 12, 90, 64)
+index_17 = num.index(17)
+print("index 17 = %d" % index_17)
